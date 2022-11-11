79b64986f72dd57ccb9d38f576ad7f12094ba844
Olga Zarosylova <zoa1906@gmail.com>
2022-11-11 17:34:49 +0200

    //yea

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        floatTypeCasting.roundNumber(5.5f);
+
+    }
     public void roundNumber(float numberToBeRounded) {
+       int round = (int) numberToBeRounded;
+       System.out.println(round);
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long thousand = 1000;
+        int thous = (int) thousand;
+        System.out.println(thousand);
+        System.out.println(thous);
     }
 }
